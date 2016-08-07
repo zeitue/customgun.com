b28ac3726d8f3a774fe0b87405b3443bf639d835
zeitue <zeitue@gmail.com>
2016-08-07 17:42:32 -0500

    Added URL validation
    Added URL validation for uniqueness
    Stripped whitespace from all text fields

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,21 +2,22 @@ class Product < ActiveRecord::Base
   include FriendlyId
   friendly_id :url
   validates :title, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true
   has_many :photos
   accepts_nested_attributes_for :photos
-  before_save :default_values
+  before_validation :default_values
 
   def default_values
-    self.title.downcase!
-    self.manufacturer.downcase!
-    self.model.downcase!
-    self.type_field.downcase!
-    self.style_field.downcase!
-    self.material.downcase!
-    self.caliber.downcase!
-    self.part_number.downcase!
-    self.url = self.title.parameterize
-    self.tags.downcase!
+    self.title = self.title.strip.downcase
+    self.manufacturer = self.manufacturer.strip.downcase
+    self.model = self.model.strip.downcase
+    self.type_field = self.type_field.strip.downcase
+    self.style_field = self.style_field.strip.downcase
+    self.material = self.material.strip.downcase
+    self.caliber = self.caliber.strip.downcase
+    self.part_number = self.part_number.strip.downcase
+    self.url = self.title.strip.parameterize
+    self.tags = self.tags.strip.downcase
   end
 
 end
