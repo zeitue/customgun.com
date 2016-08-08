90dbb3d989abf8d05157aa36a39ef86031c1ac56
zeitue <zeitue@gmail.com>
2016-08-07 21:43:36 -0500

    Fixed redirect on sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,4 +7,12 @@ class RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     products_path
   end
+
+  def after_sign_up_path_for(resource)
+     products_path
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+     products_path
+  end
 end
