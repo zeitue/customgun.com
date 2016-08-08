caf700bb3e0731b4585b7190fce9001b905a867b
zeitue <zeitue@gmail.com>
2016-08-07 21:32:40 -0500

    Added confirmable to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625061037) do
+ActiveRecord::Schema.define(version: 20160808021800) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "name"
@@ -104,8 +104,13 @@ ActiveRecord::Schema.define(version: 20160625061037) do
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
     t.boolean  "guest"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
