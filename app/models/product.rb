f0d9e18aaa24cce76936bf5479b2a71d3f2bf280
zeitue <zeitue@gmail.com>
2016-10-22 22:49:04 -0500

    Added safety for nil class

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,23 +10,23 @@ class Product < ActiveRecord::Base
 
 
   def default_values
-    self.title = self.title.strip.downcase
-    self.manufacturer = self.manufacturer.strip.downcase
-    self.model = self.model.strip.downcase
-    self.type_field = self.type_field.strip.downcase
-    self.style_field = self.style_field.strip.downcase
-    self.material = self.material.strip.downcase
-    self.caliber = self.caliber.strip.downcase
-    self.part_number = self.part_number.strip.downcase
+    self.title = self.title.to_s.strip.downcase
+    self.manufacturer = self.manufacturer.to_s.strip.downcase
+    self.model = self.model.to_s.strip.downcase
+    self.type_field = self.type_field.to_s.strip.downcase
+    self.style_field = self.style_field.to_s.strip.downcase
+    self.material = self.material.to_s.strip.downcase
+    self.caliber = self.caliber.to_s.strip.downcase
+    self.part_number = self.part_number.to_s.strip.downcase
     self.url = self.title.strip.parameterize
-    self.tags = self.tags.strip.downcase
-    self.shipped_by = self.shipped_by.blank? ? nil : self.shipped_by.strip.downcase
-    self.diopter_adjustment = self.diopter_adjustment.strip.downcase
-    self.elevation_travel = self.elevation_travel.strip.downcase
-    self.windage_travel = self.windage_travel.strip.downcase
-    self.total_travel = self.total_travel.strip.downcase
-    self.position_of_reticle = self.position_of_reticle.strip.downcase
-    self.available_reticles = self.available_reticles.strip.downcase
+    self.tags = self.tags.to_s.strip.downcase
+    self.shipped_by = self.shipped_by.blank? ? nil : self.shipped_by.to_s.strip.downcase
+    self.diopter_adjustment = self.diopter_adjustment.to_s.strip.downcase
+    self.elevation_travel = self.elevation_travel.to_s.strip.downcase
+    self.windage_travel = self.windage_travel.to_s.strip.downcase
+    self.total_travel = self.total_travel.to_s.strip.downcase
+    self.position_of_reticle = self.position_of_reticle.to_s.strip.downcase
+    self.available_reticles = self.available_reticles.to_s.strip.downcase
   end
 
 
