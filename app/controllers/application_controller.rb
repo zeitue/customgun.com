41578a49d1dd1f90fc58fad6139e0ba8f68e3b03
zeitue <zeitue@gmail.com>
2017-01-12 00:53:02 -0600

    Upgraded Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -163,14 +163,7 @@ class ApplicationController < ActionController::Base
   def configure_devise_permitted_parameters
     registration_params = [:name, :email, :password, :password_confirmation]
 
-    if params[:action] == 'update'
-      devise_parameter_sanitizer.for(:account_update) do |u|
-        u.permit(registration_params << :current_password)
-      end
-    elsif params[:action] == 'create'
-      devise_parameter_sanitizer.for(:sign_up) do |u|
-        u.permit(registration_params)
-      end
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: registration_params)
+    devise_parameter_sanitizer.permit(:account_update, keys: registration_params)
   end
 end
